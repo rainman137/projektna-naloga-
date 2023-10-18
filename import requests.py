d4d34f8a8396d7460a6f603bd69a55b4df1a6a27
rainman137 <144933695+rainman137@users.noreply.github.com>
2023-10-18 19:50:00 +0100

    Update import requests.py

--- import requests.py
+++ import requests.py
@@ -4,12 +4,12 @@ from ekipe import seznam_ekip
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0"}
 
-
 stran = requests.get(
     "https://www.2kratings.com/teams/dallas-mavericks",
     headers=headers
 )
 
+#S tem delom kode prenesemo html vseh nba ekip na spletni strani.
 with open("{ekipa}.html", "w", encoding="UTF8") as dat:
     dat.write(stran.text)
 for ekipa in seznam_ekip:
