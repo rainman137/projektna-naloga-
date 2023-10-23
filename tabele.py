6e3e10bb1ab9619da1acf230f3e5d280207a3dc5
rainman137 <144933695+rainman137@users.noreply.github.com>
2023-10-23 19:07:28 +0100

    Update tabele.py

--- tabele.py
+++ tabele.py
@@ -7,3 +7,19 @@ import funkcije
 for ekipa in seznam_ekip:
     sez = funkcije.najdi_podetke_igralcev(ekipa)
     funkcije.napisi_csv(sez, ekipa)
+
+for ekipa in seznam_ekip:
+    igralci = funkcije.dobi_seznam_igralcev(f"Informacije_o_igralcih_{ekipa}.csv")
+    atributi = funkcije.dobi_seznam_atributov(igralci[0])
+    at = ["ime"] + atributi
+    print(ekipa)
+    with open(f"Informacije_o_igralcih111_{ekipa}.csv", "w", newline="") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(at)
+    for igralec in igralci:
+        vrednost = funkcije.dobi_vrednosti(igralec)
+        vre = [igralec] + vrednost
+        with open(f"Informacije_o_igralcih111_{ekipa}.csv", "a", newline="") as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(vre)
+            print(igralec)
