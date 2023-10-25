f3e57ec29ace81bc4256dfe8f196d0b5d0db6bae
rainman137 <144933695+rainman137@users.noreply.github.com>
2023-10-25 17:33:50 +0100

    Update tabele.py
    Posodobitev funkcij: prej so se za vsako ekipo vrednosti zapisovale v svoj csv, sedaj pa s evse v enega, igralcem je dodana tudi ekipa

--- tabele.py
+++ tabele.py
@@ -5,23 +5,33 @@ import import_requests
 import requests
 import csv
 
-# Load the HTML content from "Atlanta-Hawks.html" or your HTML file
+
+#napise imena igralcev v csv datoteko
 for ekipa in seznam_ekip:
-    sez = funkcije.najdi_podetke_igralcev(ekipa)
-    funkcije.napisi_csv(sez, ekipa)
+    sez = funkcije.najdi_imena_igralcev(ekipa)
+    funkcije.napisi_imena_csv(sez, ekipa)
+
+
+atributi = funkcije.dobi_seznam_atributov("luka-doncic")
+at = ["ime", "položaj", "ekipa"] + atributi
+with open(f"Informacije_o_igralcih.csv", "w", newline="") as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(at)
+
 
 for ekipa in seznam_ekip:
     igralci = funkcije.dobi_seznam_igralcev(f"Informacije_o_igralcih_{ekipa}.csv")
-    atributi = funkcije.dobi_seznam_atributov(igralci[0])
-    at = ["ime"] + atributi
+    polozaji = funkcije.dobi_seznam_plozajev(f"Informacije_o_igralcih_{ekipa}.csv")
     print(ekipa)
-    with open(f"Informacije_o_igralcih111_{ekipa}.csv", "w", newline="") as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(at)
     for igralec in igralci:
         vrednost = funkcije.dobi_vrednosti(igralec)
-        vre = [igralec] + vrednost
-        with open(f"Informacije_o_igralcih111_{ekipa}.csv", "a", newline="") as csvfile:
-            csv_writer = csv.writer(csvfile)
-            csv_writer.writerow(vre)
-            print(igralec)
+        if len(vrednost) != 0:
+            x = vrednost.pop(26)
+            x = vrednost[33:39 + 1] #Ker so se nekatere vrednosti zapisovale 2x, jih s tem jih odstranimo
+            vrednost[33:39 + 1] = []
+            index = igralci.index(igralec)
+            pos = polozaji[index]
+            vre = [igralec, pos.strip('[]'), ekipa] + vrednost        
+            with open(f"Informacije_o_igralcih.csv", "a", newline="") as csvfile:
+                csv_writer = csv.writer(csvfile)
+                csv_writer.writerow(vre)
