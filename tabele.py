aece429bdd40b9914ac2f763821c57f0aa602850
rainman137 <144933695+rainman137@users.noreply.github.com>
2023-10-26 20:20:46 +0100

    Update tabele.py
    Olepševanje kode

--- tabele.py
+++ tabele.py
@@ -1,37 +1,34 @@
-from bs4 import BeautifulSoup
+import csv
 from ekipe import seznam_ekip
 import funkcije
-import import_requests
-import requests
-import csv
 
 
-#Napise imena igralcev v csv datoteko
+# Napiše imena igralcev v CSV datoteko
 for ekipa in seznam_ekip:
     sez = funkcije.najdi_imena_igralcev(ekipa)
     funkcije.napisi_imena_csv(sez, ekipa)
 
-#Zapiše prvo vrstco csv datoteke
+# Zapiše prvo vrstico CSV datoteke
 atributi = funkcije.dobi_seznam_atributov("luka-doncic")
 at = ["ime", "polozaj", "ekipa"] + atributi
 with open(f"Informacije_o_igralcih.csv", "w", newline="") as csvfile:
     csv_writer = csv.writer(csvfile)
     csv_writer.writerow(at)
 
-#V csv datoteko napiše igralce, ter vrednosti
+# V CSV datoteko napiše igralce in vrednosti
 for ekipa in seznam_ekip:
     igralci = funkcije.dobi_seznam_igralcev(f"Informacije_o_igralcih_{ekipa}.csv")
-    polozaji = funkcije.dobi_seznam_plozajev(f"Informacije_o_igralcih_{ekipa}.csv")
+    polozaji = funkcije.dobi_seznam_polozajev(f"Informacije_o_igralcih_{ekipa}.csv")
     print(ekipa)
     for igralec in igralci:
         vrednost = funkcije.dobi_vrednosti(igralec)
         if len(vrednost) != 0:
             x = vrednost.pop(26)
-            x = vrednost[33:39 + 1] #Ker so se nekatere vrednosti zapisovale 2x, jih s tem jih odstranimo
+            x = vrednost[33:39 + 1]  # Ker so se nekatere vrednosti zapisovale 2x, jih s tem jih odstranimo
             vrednost[33:39 + 1] = []
             index = igralci.index(igralec)
             pos = polozaji[index]
-            vre = [igralec, pos.strip('[]'), ekipa] + vrednost        
+            vre = [igralec, pos.strip('[]'), ekipa] + vrednost
             with open(f"Informacije_o_igralcih.csv", "a", newline="") as csvfile:
                 csv_writer = csv.writer(csvfile)
                 csv_writer.writerow(vre)
