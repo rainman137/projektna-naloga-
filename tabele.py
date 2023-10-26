b63fc945569311c684cfd632a7e233bd2dc00335
rainman137 <144933695+rainman137@users.noreply.github.com>
2023-10-26 21:37:33 +0100

    Update tabele.py
    Zadnja sprememba močno izboljša delovanje programa, saj se izogne pisanju odvečnih csv datotek

--- tabele.py
+++ tabele.py
@@ -2,12 +2,6 @@ import csv
 from ekipe import seznam_ekip
 import funkcije
 
-
-# Napiše imena igralcev v CSV datoteko
-for ekipa in seznam_ekip:
-    sez = funkcije.najdi_imena_igralcev(ekipa)
-    funkcije.napisi_imena_csv(sez, ekipa)
-
 # Zapiše prvo vrstico CSV datoteke
 atributi = funkcije.dobi_seznam_atributov("luka-doncic")
 at = ["ime", "polozaj", "ekipa"] + atributi
@@ -17,18 +11,16 @@ with open(f"Informacije_o_igralcih.csv", "w", newline="") as csvfile:
 
 # V CSV datoteko napiše igralce in vrednosti
 for ekipa in seznam_ekip:
-    igralci = funkcije.dobi_seznam_igralcev(f"Informacije_o_igralcih_{ekipa}.csv")
-    polozaji = funkcije.dobi_seznam_polozajev(f"Informacije_o_igralcih_{ekipa}.csv")
+    igralci = funkcije.najdi_imena_igralcev(ekipa)
     print(ekipa)
-    for igralec in igralci:
-        vrednost = funkcije.dobi_vrednosti(igralec)
+    for igralec, polozaj in igralci:
+        igralec_link = igralec.replace(' ', '-')
+        vrednost = funkcije.dobi_vrednosti(igralec_link)
         if len(vrednost) != 0:
             x = vrednost.pop(26)
             x = vrednost[33:39 + 1]  # Ker so se nekatere vrednosti zapisovale 2x, jih s tem jih odstranimo
             vrednost[33:39 + 1] = []
-            index = igralci.index(igralec)
-            pos = polozaji[index]
-            vre = [igralec, pos.strip('[]'), ekipa] + vrednost
+            vre = [igralec, polozaj, ekipa] + vrednost
             with open(f"Informacije_o_igralcih.csv", "a", newline="") as csvfile:
                 csv_writer = csv.writer(csvfile)
                 csv_writer.writerow(vre)
